a36abec34f88ec3fb775c6158e87b980110de508
Ethan Ho <erh101@case.edu>
2023-12-21 10:07:52 -0500

    dark theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import { Leaderboard } from './components/Leaderboard';
 import { Description } from './components/Description';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-        <h1>Framed Tournament Current Leaderboard</h1>
-        <Leaderboard/>
-        <Description />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+          <h1>Framed Tournament Current Leaderboard</h1>
+          <Leaderboard/>
+          <Description />
       </div>
-
+    </ThemeProvider>
   );
 }
 
